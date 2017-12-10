13bb6151f9ce441d401e0454ff246239df23074c
花田暁 <hanadaakira@hanadaakiranoMacBook-Air.local>
2017-12-10 13:05:21 +0900

    users controllerのedit,updateアクションの追加

--- chat-space/app/controllers/users_controller.rb
+++ chat-space/app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
 
   def edit
-
+    @user = User.find(params[:id]) if user.id == current_user.id
   end
 
   def update
-
+    user =User.find(params[:id])
+      user.update(user_params)
+    end
   end
+
+Private
+def user_params
+  params.permit(:name,:email)
 end
