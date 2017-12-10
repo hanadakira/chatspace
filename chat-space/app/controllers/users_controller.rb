1de280339b684a87a50a1f2140d8497041c38d67
花田暁 <hanadaakira@hanadaakiranoMacBook-Air.local>
2017-12-10 13:17:23 +0900

    編集後トップページに戻る

--- chat-space/app/controllers/users_controller.rb
+++ chat-space/app/controllers/users_controller.rb
@@ -6,11 +6,16 @@ class UsersController < ApplicationController
 
   def update
     user =User.find(params[:id])
-      user.update(user_params)
-    end
+    user.update(user_params)
+    move_to_index
   end
 
-Private
-def user_params
-  params.permit(:name,:email)
+  private
+  def user_params
+    params.permit(:name,:email)
+  end
+
+  def move_to_index
+    redirect_to controller: :messages, action: :index
+  end
 end
