2fee1cd85ec101474c9d5b00137d7e18c3709caf
花田暁 <hanadaakira@hanadaakiranoMacBook-Air.local>
2017-12-14 08:46:58 +0900

    Group controllerの設定

--- chat-space/app/controllers/groups_controller.rb
+++ chat-space/app/controllers/groups_controller.rb
@@ -1,4 +1,9 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit,:update]
+
+  def index
+    @groups = current_user.groups.order('id DESC')
+  end
 
   def new
     @group = Group.new
@@ -17,9 +22,18 @@ class GroupsController < ApplicationController
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to :root,notice: 'グループを編集しました。'
+      else
+        render :edit,alert: '失敗しました。'
+      end
   end
 
   def group_params
     params.require(:group).permit(:name,{user_ids: []})
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
