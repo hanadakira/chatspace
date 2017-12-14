21ad355e64a856f3c9f3d686671a5d2efabe22dc
花田暁 <hanadaakira@hanadaakiranoMacBook-Air.local>
2017-12-14 17:07:29 +0900

    コントローラーの設定

--- chat-space/app/controllers/messages_controller.rb
+++ chat-space/app/controllers/messages_controller.rb
@@ -1,5 +1,21 @@
 class MessagesController < ApplicationController
   def index
     @groups = current_user.groups.order('id DESC')
+    @message = Message.new
+    @group = Group.find(16)
   end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to :root,notice: 'メッセージが送信されました。'
+    else
+      redirect_to :root, alert: 'メッセージが送信できませんでした。'
+    end
+  end
+
+  def message_params
+    params.permit(:body,:image,:user_id,:group_id)
+  end
+
 end
