7170cfd251fb317d4f0e2f45173e466529f03a62
花田暁 <hanadaakira@hanadaakiranoMacBook-Air.local>
2017-12-15 15:26:13 +0900

    DB編集

--- chat-space/db/schema.rb
+++ chat-space/db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 20171214025459) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id",    null: false
-    t.integer  "group_id",   null: false
+    t.integer  "user_id"
+    t.integer  "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_group_users_on_group_id", using: :btree
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20171214025459) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",                   default: "", null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -49,8 +50,8 @@ ActiveRecord::Schema.define(version: 20171214025459) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name",                   default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["name"], name: "index_users_on_name", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
