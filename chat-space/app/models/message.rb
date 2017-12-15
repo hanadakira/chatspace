7cba179fa5ecb4b9a4ff57f5c9e3bc939fcb2231
花田暁 <hanadaakira@hanadaakiranoMacBook-Air.local>
2017-12-15 17:08:21 +0900

    バリデーション メッセージ送信時に空欄で送信できないように設定

--- chat-space/app/models/message.rb
+++ chat-space/app/models/message.rb
@@ -2,5 +2,9 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
   mount_uploader :image,ImageUploader
-  validates :
+  validates :body_image_present,presence: :true
+
+  def body_image_present
+    body.presence || image.presence
+  end
 end
