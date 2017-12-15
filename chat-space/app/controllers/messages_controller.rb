cdca61d6efc8060ee24f907fa0967549145d2682
花田暁 <hanadaakira@hanadaakiranoMacBook-Air.local>
2017-12-15 15:24:55 +0900

    message controllerの設定

--- chat-space/app/controllers/messages_controller.rb
+++ chat-space/app/controllers/messages_controller.rb
@@ -1,21 +1,23 @@
 class MessagesController < ApplicationController
+
   def index
     @groups = current_user.groups.order('id DESC')
     @message = Message.new
-    @group = Group.find(16)
+    @group = Group.find(params[:group_id])
+    @messages = Message.includes(:group).order("created_at DESC")
   end
 
   def create
-    @message = Message.new(message_params)
+    @message = Message.new(message_params.merge(user_id: current_user.id,group_id: params[:group_id]))
     if @message.save
-      redirect_to :root,notice: 'メッセージが送信されました。'
+      redirect_to({action: :index, group_id:params[:group_id]},notice: 'メッセージが送信されました。')
     else
       redirect_to :root, alert: 'メッセージが送信できませんでした。'
     end
   end
 
   def message_params
-    params.permit(:body,:image,:user_id,:group_id)
+    params.require(:message).permit(:body,:image)
   end
 
 end
