084734791e173ba8cf5f4dd0fb7a62decc599fe1
花田暁 <hanadaakira@hanadaakiranoMacBook-Air.local>
2017-12-15 17:06:54 +0900

    redirect_to パスで指定 strong parameterを編集

--- chat-space/app/controllers/messages_controller.rb
+++ chat-space/app/controllers/messages_controller.rb
@@ -8,16 +8,17 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = Message.new(message_params.merge(user_id: current_user.id,group_id: params[:group_id]))
+    @message = Message.new(message_params)
     if @message.save
-      redirect_to({action: :index, group_id:params[:group_id]},notice: 'メッセージが送信されました。')
+      redirect_to group_messages_path(group_id:params[:group_id]),notice: 'メッセージが送信されました。'
     else
       redirect_to :root, alert: 'メッセージが送信できませんでした。'
     end
   end
 
+  private
   def message_params
-    params.require(:message).permit(:body,:image)
+    params.require(:message).permit(:body,:image).merge(user_id: current_user.id,group_id: params[:group_id])
   end
 
 end
