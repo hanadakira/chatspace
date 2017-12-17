51c7ef11fca5ce8740bba720aac473b763c0c7ec
花田暁 <hanadaakira@hanadaakiranoMacBook-Air.local>
2017-12-18 07:55:58 +0900

    メッセージモデルの：の削除

--- chat-space/app/models/message.rb
+++ chat-space/app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
   mount_uploader :image,ImageUploader
-  validates :body_image_present,presence: :true
+  validates :body_image_present, presence: true
 
   def body_image_present
     body.presence || image.presence
