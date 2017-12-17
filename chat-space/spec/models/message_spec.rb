145357e14561e229b174623dc8e9f54af478bc44
花田暁 <hanadaakira@hanadaakiranoMacBook-Air.local>
2017-12-18 07:58:51 +0900

    モデルのテストコードの記述

---
+++ chat-space/spec/models/message_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe "message" do
+  describe '#create' do
+    it "メッセージを保存できる" do
+      # binding.pry
+      message = build(:message)
+      expect(message).to be_valid
+    end
+  end
+   describe '#create' do
+    it "画像があれば保存できる" do
+      message = build(:message, body: nil)
+      expect(message).to be_valid
+    end
+  end
+   describe '#create' do
+    it "メッセージがあれば保存できる" do
+      message = build(:message, image: nil)
+      expect(message).to be_valid
+    end
+  end
+  describe '#create' do
+    it "メッセージを保存できる" do
+      message = build(:message, image: "",body: "")
+      message.valid?
+      expect(message.errors[:body_image_present]).to include("を入力してください")
+    end
+  end
+  describe '#create' do
+    it "group_idがないと保存できない" do
+      # binding.pry
+      message = build(:message, group: nil)
+      message.valid?
+      expect(message.errors[:group]).to include("を入力してください")
+    end
+  end
+  describe '#create' do
+    it "user_idがないと保存できない" do
+      message = build(:message, user: nil)
+      message.valid?
+      expect(message.errors[:user]).to include("を入力してください")
+    end
+  end
+end
