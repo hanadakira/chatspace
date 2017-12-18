74accd3c46a699452943f0433060fb5c98e2d9d4
花田暁 <hanadaakira@hanadaakiranoMacBook-Air.local>
2017-12-18 12:23:31 +0900

    無駄なコードの削除

--- chat-space/spec/models/message_spec.rb
+++ chat-space/spec/models/message_spec.rb
@@ -6,35 +6,25 @@ describe "message" do
       message = build(:message)
       expect(message).to be_valid
     end
-  end
-   describe '#create' do
     it "画像があれば保存できる" do
       message = build(:message, body: nil)
       expect(message).to be_valid
     end
-  end
-   describe '#create' do
     it "メッセージがあれば保存できる" do
       message = build(:message, image: nil)
       expect(message).to be_valid
     end
-  end
-  describe '#create' do
     it "メッセージも画像もないと保存できない" do
       message = build(:message, image: "",body: "")
       message.valid?
       expect(message.errors[:body_image_present]).to include("を入力してください")
     end
-  end
-  describe '#create' do
     it "group_idがないと保存できない" do
       # binding.pry
       message = build(:message, group: nil)
       message.valid?
       expect(message.errors[:group]).to include("を入力してください")
     end
-  end
-  describe '#create' do
     it "user_idがないと保存できない" do
       message = build(:message, user: nil)
       message.valid?
