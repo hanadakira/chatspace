9fb792548dc04b36a500468f0cf816f08d9e2abe
花田暁 <hanadaakira@hanadaakiranoMacBook-Air.local>
2017-12-19 17:35:51 +0900

    controllerのテスト

--- chat-space/spec/factories/messages.rb
+++ chat-space/spec/factories/messages.rb
@@ -5,5 +5,6 @@ FactoryGirl.define do
     image    Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/image2.jpeg'))
     group
     user
+    created_at { Faker::Time.between(2.days.ago, Time.now, :all) }
   end
 end
