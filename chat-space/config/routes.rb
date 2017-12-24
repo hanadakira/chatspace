ff4a4c2c2ba71414e73569b5312a480d52f2c5f4
花田暁 <hanadaakira@hanadaakiranoMacBook-Air.local>
2017-12-24 14:32:26 +0900

    ルーティングの設定

--- chat-space/config/routes.rb
+++ chat-space/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 	root "groups#index"
+  resources :users, only: [:index,:edit,:update]
   resources :groups, only: [:index,:new, :create, :update, :edit] do
     resources :messages
   end
