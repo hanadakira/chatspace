c7ee964604fda356fc61ead497618a3a19fcce44
花田暁 <hanadaakira@hanadaakiranoMacBook-Air.local>
2017-12-25 22:18:44 +0900

    コントローラー

--- chat-space/app/controllers/messages_controller.rb
+++ chat-space/app/controllers/messages_controller.rb
@@ -1,15 +1,18 @@
 class MessagesController < ApplicationController
 
+before_action :set_group, only: [:index,:new,:create]
+
   def index
     @groups = current_user.groups.order('id DESC')
     @message = Message.new
-    @group = Group.find(params[:group_id])
-    @messages = Message.includes(:group).order("created_at DESC")
+    @messages = @group.messages.includes(:user)
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def create
-    # binding.pry
-    @group = Group.find(params[:group_id])
     @message = Message.new(message_params)
     if @message.save
       respond_to do |format|
@@ -22,8 +25,11 @@ class MessagesController < ApplicationController
   end
 
   private
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
   def message_params
-    params.require(:message).permit(:body,:image).merge(user_id: current_user.id,group_id: params[:group_id])
+    params.require(:message).permit(:body,:image,:group_id,:image).merge(user_id: current_user.id,group_id: params[:group_id])
   end
 
 end
